7574e9b9785295dabbe59ff1925698f3b9f26fd8
prajwal <prajwalgiri02@gmail.com>
2026-01-21 20:45:59 +0545

    Finalize composer.json

--- src/CrudGeneratorServiceProvider.php
+++ src/CrudGeneratorServiceProvider.php
@@ -1,33 +1,40 @@
 <?php
 
-namespace prajwal\CrudGenerator;
+namespace Prajwal\CrudGenerator;
 
 use Illuminate\Support\ServiceProvider;
-use prajwal\CrudGenerator\Commands\CrudGenerateCommand;
-use prajwal\CrudGenerator\Commands\CrudInstallCommand;
+use Prajwal\CrudGenerator\Commands\CrudGenerateCommand;
+use Prajwal\CrudGenerator\Commands\CrudInstallCommand;
 
 class CrudGeneratorServiceProvider extends ServiceProvider
 {
-    public function boot()
+    public function boot(): void
     {
         $this->publishes([
-            __DIR__ . '/config/crud-forms.php' => config_path('crud-forms.php'),
+            $this->packagePath('config/crud-forms.php') => config_path('crud-forms.php'),
         ], 'crud-config');
 
         $this->publishes([
-            __DIR__ . '/Stubs' => base_path('stubs/crud-generator'),
+            $this->packagePath('stubs') => base_path('stubs/crud-generator'),
         ], 'crud-stubs');
     }
 
-    public function register()
+    public function register(): void
     {
         $this->mergeConfigFrom(
-            __DIR__ . '/config/crud-forms.php', 'crud-forms'
+            $this->packagePath('config/crud-forms.php'),
+            'crud-forms'
         );
 
         $this->commands([
             CrudGenerateCommand::class,
             CrudInstallCommand::class,
         ]);
+    }
+
+    private function packagePath(string $path): string
+    {
+        // Package root = one level above /src
+        return dirname(__DIR__) . '/' . ltrim($path, '/');
     }
 }
